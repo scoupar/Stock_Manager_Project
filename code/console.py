1482488c81c853e0221cfa2dda7572ece3c924f1
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-29 20:40:33 +0000

    finished supplier repository

--- code/console.py
+++ code/console.py
@@ -4,9 +4,15 @@ from models.supplier import Supplier
 
 import repositories.supplier_repository as supplier_repository
 
-supplier_repository.delete_all()
+# supplier_repository.delete_all()
 
-supplier1 = Supplier("Dunns", "0141 1234567", "must order before 5PM")
-supplier_repository.save(supplier1)
+# supplier1 = Supplier("Dunns", "0141 1234567", "must order before 5PM")
+# supplier_repository.save(supplier1)
 
-pdb.set_trace()
+# supplier2 = Supplier("Rodgers", "0141 234546", "no deliveries on a Sunday")
+# supplier_repository.save(supplier2)
+
+# print(supplier_repository.select_all())
+
+# print(supplier_repository.select(15))
+# pdb.set_trace()
--- code/repositories/supplier_repository.py
+++ code/repositories/supplier_repository.py
@@ -14,3 +14,26 @@ def save(supplier):
 def delete_all():
     sql = "DELETE FROM suppliers"
     run_sql(sql)
+
+def select_all():
+    suppliers = []
+    sql = "SELECT * FROM suppliers"
+    results = run_sql(sql)
+    for row in results:
+        supplier = Supplier(row['supplier_name'], row['contact_info'], row['id'])
+        suppliers.append(supplier)
+    return suppliers
+
+def select(id):
+    supplier = None
+    sql ="SELECT * FROM suppliers WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        supplier = Supplier(result['supplier_name'], result['contact_info'], result['id'])
+    return supplier
+
+def update(supplier):
+    sql ="UPDATE suppliers SET (supplier_name, contact_info, notes) = (%s, %s, %s) WHERE id =%s"
+    values = [supplier.supplier_name, supplier.contact_info, supplier.notes]
+    run_sql(sql, values)
